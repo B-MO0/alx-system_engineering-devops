7664bed2e8de05f530fbd1dea96040f3d40d15f7
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2024-04-20 01:59:30 +0100

    cmo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
 # Install a package
-exec { 'install_flask_2.1.0':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/bin', '/usr/bin'],
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  environment => ['PIP_DISABLE_PIP_VERSION_CHECK=1'],
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
