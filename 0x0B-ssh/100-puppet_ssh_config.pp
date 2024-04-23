e7c46d2bbe7243c544f1e2202117de9804b9d2ee
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-04-23 15:10:53 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Use Puppet to set up configuration file to connect to server without password
+
+file { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  content => 'PasswordAuthentication no
+  IdentityFile ~/.ssh/school'
+}
